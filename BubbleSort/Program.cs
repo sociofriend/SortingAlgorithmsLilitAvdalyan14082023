c851bc33d302db7f40ec038850649384ac1e93dd
sociofriend <lilit_avdalyan@yahoo.com>
2023-08-14 11:58:35 +0400

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using BubbleSortLib;
+
+namespace SortingProject;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //given array of integers
+        int[] arr = { 1, 20, 3, 4, 5, 60, 7, 8, 9, };
+        
+        //sort the given array with bubbleSort approach
+        arr = RunBubbleSort(arr);
+        
+        //print array
+        foreach (int i in arr) 
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    /// <summary>
+    /// Creates create current instance of the class and invokes corresponding sorting method.
+    /// </summary>
+    /// <param name="array">Array of integer type elements.</param>
+    /// <returns>Returns array of integers representing the sorted given array.</returns>
+    static int[] RunBubbleSort(int[] array)
+    {
+        //create instance of the class
+        BubbleSortLogic bubbleSortLogic = new BubbleSortLogic();
+        
+        //invoke sorting method and return the sorted array
+        return bubbleSortLogic.Sort(array);
+    }
+    
+}
---
+++ BubbleSortLib/BubbleSortLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortLib;
+
+public class BubbleSortLogic
+{
+    /// <summary>
+    /// Sorts given array in bubbleSort approach.
+    /// </summary>
+    /// <param name="array">Given array of integers.</param>
+    /// <returns>Returns sorted array.</returns>
+    public int[] Sort(int[] array)
+    {
+        //maximum index  for comparison
+        int indexMax = array.Length;
+        
+        //current element of the array
+        int current;
+
+        //loop starting from index 0 to the first element of the second part of the array.
+        while (indexMax > (indexMax / 2 + 1) )
+        { 
+            for (int i = 0; i < indexMax; i++)
+            {
+                // if next element is greater than the current item, swap
+                if (i + 1 < indexMax &&
+                    array[i] > array[i + 1])
+                {
+                    current = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = current;
+                }
+            }
+            //decrement max index, as the last element of the is already the biggest
+            indexMax--;
+        }
+        return array;
+    }
+
+}
